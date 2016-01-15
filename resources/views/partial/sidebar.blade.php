a1ac90c24d341a3add7ab0af2431757fca84fbb4
abdul <abdul.hamid@tlab.co.id>
2016-01-15 08:03:58 +0700

    edit sidebar

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -4,27 +4,29 @@
     <section class="sidebar">
 
         <ul class="sidebar-menu">
-
-            {{-- Untuk tampilan menu dropdown --}}
-            {{--<li class="treeview {!! $segment == 'report-attendace' || $segment == 'report-salary' ? 'active' : '' !!}">--}}
-            {{--<a href="#">--}}
-            {{--<i class="fa fa-pie-chart"></i>--}}
-            {{--<span>Laporan</span>--}}
-            {{--<i class="fa fa-angle-left pull-right"></i>--}}
-            {{--</a>--}}
-            {{--<ul class="treeview-menu">--}}
-            {{--<li class="{!! $segment == 'report-attendace' ? 'active-treeview' : '' !!}">--}}
-            {{--<a href="{!! url('report-attendace') !!}">--}}
-            {{--<i class="fa fa-line-chart"></i> <span>Laporan Kehadiran</span>--}}
-            {{--</a>--}}
-            {{--</li>--}}
-            {{--<li class="{!! $segment == 'report-salary' ? 'active-treeview' : '' !!}" style="display:none">--}}
-            {{--<a href="{!! url('report-salary') !!}">--}}
-            {{--<i class="fa fa-line-chart"></i> <span>Laporan Gaji</span>--}}
-            {{--</a>--}}
-            {{--</li>--}}
-            {{--</ul>--}}
-            {{--</li>--}}
+            <li class="header">Manajemen Dat</li>
+            <li class="treeview {!! $segment == 'reports' ? 'active' : '' !!}">
+                <?php $segmentReport = Request::segment(2); ?>
+                <a href="#">
+                    <i class="fa fa-area-chart"></i>
+                    <span>Master</span>
+                    <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                    <li class="{!! $segment == 'groups' ? 'active' : '' !!}">
+                        <a href="{!! url('/groups') !!}">
+                            <i class="fa fa-bar-chart"></i>
+                            <span>Groups</span>
+                        </a>
+                    </li>
+                    <li class="{!! $segment == 'users'  ? 'active' : '' !!}">
+                        <a href="{!! url('/users') !!}">
+                            <i class="fa fa-bar-chart"></i>
+                            <span>Users</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
             <li class="header">KONFIGURASI</li>
             <li class="{!! $segment == 'configuration' ? 'active' : '' !!}">
                 <a href="{!! url('configuration') !!}">
@@ -37,25 +39,7 @@
                 </a>
             </li>
 
-            {{--<li class="treeview {!! $segment == 'module' || $segment == 'module_access' || $segment == 'configuration' ? 'active' : '' !!}">--}}
-                {{--<a href="#">--}}
-                    {{--<i class="fa  fa-gear"></i>--}}
-                    {{--<span>Konfigurasi</span>--}}
-                    {{--<i class="fa fa-angle-left pull-right"></i>--}}
-                {{--</a>--}}
-                {{--<ul class="treeview-menu">--}}
-                    {{--<li class="{!! $segment == 'configuration' ? 'active' : '' !!}">--}}
-                        {{--<a href="{!! url('configuration') !!}">--}}
-                            {{--<i class="fa fa-pie-chart"></i> <span> Konfigurasi Umum</span>--}}
-                        {{--</a>--}}
-                    {{--</li>--}}
-                    {{--<li class=" {!! $segment == 'module_access' ? 'active-treeview' : '' !!} " style="color: black;">--}}
-                        {{--<a href="{!! url('module_access') !!}">--}}
-                            {{--<i class="fa fa-asterisk"></i> <span>Modul Akses</span>--}}
-                        {{--</a>--}}
-                    {{--</li>--}}
-                {{--</ul>--}}
-            {{--</li>--}}
+
         </ul>
     </section>
     <!-- /.sidebar -->
