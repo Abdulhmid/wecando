8afdced04efc2fedb6354d4e44ef66c125f6eb1d
abdul <abdul.hamid@tlab.co.id>
2016-01-27 06:32:57 +0700

    editing

--- resources/views/module/member_admin/index.blade.php
+++ resources/views/module/member_admin/index.blade.php
@@ -3,41 +3,9 @@
 
 @section('content')
     <div class="row">
-        <div class="col-xs-6 col-sm-6 col-md-4 portfolio-item branded logos">
-            <div class="portfolio-wrapper">
-                <div class="portfolio-single">
-                    <div class="portfolio-thumb">
-                        <img src="{{ url('additional/home/images/portfolio/3.jpg')}}" class="img-responsive" alt="">
-                    </div>
-                    <div class="portfolio-view">
-                        <ul class="nav nav-pills">
-                            <li><a href="{!! url('me-detail-campaign') !!}"><i class="fa fa-link"></i></a></li>
-                            <li><a href="images/portfolio/1.jpg" data-lightbox="example-set"><i class="fa fa-eye"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="portfolio-info ">
-                    <h2>Sailing Vivamus </h2>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-6 col-sm-6 col-md-4 portfolio-item branded folio">
-            <div class="portfolio-wrapper">
-                <div class="portfolio-single">
-                    <div class="portfolio-thumb">
-                        <img src="{{ url('additional/home/images/portfolio/3.jpg')}}" class="img-responsive" alt="">
-                    </div>
-                    <div class="portfolio-view">
-                        <ul class="nav nav-pills">
-                            <li><a href="{!! url('me-detail-campaign') !!}"><i class="fa fa-link"></i></a></li>
-                            <li><a href="images/portfolio/2.jpg" data-lightbox="example-set"><i class="fa fa-eye"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="portfolio-info ">
-                    <h2>Sailing Vivamus</h2>
-                </div>
-            </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 portfolio-item branded logos">
+            <h1>Selamat Datang di Dashboard Fundraising <u> <b>We Can Do</b> </u></h1><br/>
+            <center><img src="{!! asset('images/default/give_together.png') !!}" class="slider-hill" alt="slider image"></center><br/>
         </div>
     </div>
 @endsection
